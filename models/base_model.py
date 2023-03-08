2d25895f8b84b36627220fd67ade98b08d58e2e4
Nathan-88 <onwukaebuka88@gmail.com>
2023-03-08 10:33:07 +0100

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
         if kwargs:
             for key, value in kwargs.items():
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.now()
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 if key != '__class__':
                     setattr(self, key, value)
         else:
