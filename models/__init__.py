36868110e840e096c9ea41ce567eecacf1c48e91
Nathan-88 <onwukaebuka88@gmail.com>
2023-03-11 12:17:00 +0100

    fixed pycodestyle errors in files

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """This module initializes the storage variable"""
+
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 import json
 
+
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
-    
-    
-    
+
     def all(self):
         """
-        Returns a dictionary of all objects stored in the file(file.json) as key,
+        Returns a dictionary of all objects stored
+        in the file(file.json) as key,
         value pairs.
         """
         return self.__objects
-    
+
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id
+        """
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
-        
+
     def save(self):
         """Saves to the file storage, updated models (objects)."""
         obj_dict = {}
@@ -25,7 +27,7 @@ class FileStorage:
             obj_dict[key] = obj.to_dict()
         with open(self.__file_path, mode="w", encoding="utf-8") as f:
             json.dump(obj_dict, f)
-            
+
     def reload(self):
         """Loads objects from file storage"""
         from models.base_model import BaseModel
