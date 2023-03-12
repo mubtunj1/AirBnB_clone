f16f7815179c541e30056be8952220947c8231df
Mubtunj1 <mubtunji01@gmail.com>
2023-03-12 05:24:49 +0100

    Improved file_storage and console

--- console.py
+++ console.py
@@ -7,7 +7,7 @@ then dispatch the command to an appropriate command handler
 """
 import cmd
 from models import *
-from models import storage
+
 
 class HBNBCommand(cmd.Cmd):
     """defines the HBNBCommand interpreter"""
@@ -27,8 +27,10 @@ class HBNBCommand(cmd.Cmd):
         """ensures an empty line + ENTER doesnt execute anything
         """
         pass
-    
+
     def do_create(self, line):
+        """Command handler creates a new instance of a class,
+        saves it to JSON file and prints id"""
         if line == "":
             print("** class name missing **")
         else:
@@ -38,15 +40,17 @@ class HBNBCommand(cmd.Cmd):
                 obj = storage.classes()[line]()
                 obj.save()
                 print(obj.id)
-                
+
     def do_show(self, line):
+        """Command handler prints the string representation
+        of an instance based on the class name and id"""
         arg = line.split()
         if line == "":
             print("** class name missing **")
         elif arg[0] not in storage.classes():
             print("** class doesn't exist **")
         elif len(arg) == 1:
-            print("** instance id missing **") 
+            print("** instance id missing **")
         else:
             obj = storage.all()
             key = f"{arg[0]}.{arg[1]}"
@@ -54,15 +58,17 @@ class HBNBCommand(cmd.Cmd):
                 print("** no instance found **")
             else:
                 print(obj[key])
-        
+
     def do_destroy(self, line):
+        """Command handler deletes an instance based on the class
+        name and id and saves the changes in a JSON file"""
         arg = line.split()
         if line == "":
             print("** class name missing **")
         elif arg[0] not in storage.classes():
             print("** class doesn't exist **")
         elif len(arg) == 1:
-            print("** instance id missing **") 
+            print("** instance id missing **")
         else:
             obj = storage.all()
             key = f"{arg[0]}.{arg[1]}"
@@ -71,13 +77,50 @@ class HBNBCommand(cmd.Cmd):
             else:
                 del(obj[key])
                 storage.save()
-                
+
     def do_all(self, line):
+        """Command handler prints all str representations
+        of all instances based or not on the class name"""
+        list = []
         arg = line.split()
-        objs= storage.all()
-        
-        
-                
-            
+        objs = storage.all()
+        if not arg:
+            for key in objs.keys():
+                list.append(str(objs[key]))
+            print(list)
+
+        elif arg[0] not in storage.classes():
+            print("** class doesn't exist **")
+        else:
+            for key in objs.keys():
+                kwarg = key.split(".")
+                if arg[0] == kwarg[0]:
+                    list.append(str(objs[key]))
+            print(list)
+
+    def do_update(self, line):
+        """Command handler updates an instance based on the class
+        name and id by adding or updating an attribute"""
+        args = line.split()
+        if not args:
+            print("** class name missing **")
+        elif args[0] not in storage.classes():
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        elif len(args) == 2:
+            print("** attribute name missing **")
+        elif len(args) == 3:
+            print("** value missing **")
+        else:
+            objs = storage.all()
+            key = "{}.{}".format(args[0], args[1])
+            if key not in objs.keys():
+                print("** no instance found **")
+            else:
+                setattr(objs[key], args[2], args[3])
+                storage.save()
+
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,23 +4,26 @@ convert the dictionary representation to a JSON string
 """
 import json
 
+
 class FileStorage:
+    """a representation of the file storage"""
     __file_path = "file.json"
     __objects = {}
-    
-    
-    
+
     def all(self):
         """
-        Returns a dictionary of all objects stored in the file(file.json) as key,
+        Returns a dictionary of all objects stored
+        in the file(file.json) as key,
         value pairs.
         """
         return self.__objects
-    
+
     def new(self, obj):
+        """sets in __objects the obj with key <obj class name>.id
+        """
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
-        
+
     def save(self):
         """Saves to the file storage, updated models (objects)."""
         obj_dict = {}
@@ -28,21 +31,22 @@ class FileStorage:
             obj_dict[key] = obj.to_dict()
         with open(self.__file_path, mode="w", encoding="utf-8") as f:
             json.dump(obj_dict, f)
-            
+
     def classes(self):
         from models.base_model import BaseModel
-
-        class_dict = {
+        classes = {
             'BaseModel': BaseModel,
         }
+        return classes
 
+    def reload(self):
+        """Loads objects from file storage"""
         try:
             with open(self.__file_path, mode="r", encoding="utf-8") as f:
                 FileStorage.__objects = json.load(f)
                 for key, value in FileStorage.__objects.items():
                     cls_name = value['__class__']
-                    if cls_name in class_dict:
-                        cls = class_dict[cls_name]
-                        FileStorage.__objects[key] = cls(**value)
+                    cls = self.classes()[cls_name]
+                    FileStorage.__objects[key] = cls(**value)
         except FileNotFoundError:
             pass
