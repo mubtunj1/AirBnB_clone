b7f86a060a09a9166b02d2b694f98cffe87162ed
Nathan-88 <onwukaebuka88@gmail.com>
2023-03-13 11:24:56 +0100

    changed self.__objects to FileStorage.__objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -16,20 +16,20 @@ class FileStorage:
         in the file(file.json) as key,
         value pairs.
         """
-        return self.__objects
+        return FileStorage.__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id
         """
         key = "{}.{}".format(type(obj).__name__, obj.id)
-        self.__objects[key] = obj
+        FileStorage.__objects[key] = obj
 
     def save(self):
         """Saves to the file storage, updated models (objects)."""
         obj_dict = {}
-        for key, obj in self.__objects.items():
+        for key, obj in FileStorage.__objects.items():
             obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, mode="w", encoding="utf-8") as f:
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as f:
             json.dump(obj_dict, f)
 
     def classes(self):
@@ -55,7 +55,7 @@ class FileStorage:
     def reload(self):
         """Loads objects from file storage"""
         try:
-            with open(self.__file_path, mode="r", encoding="utf-8") as f:
+            with open(FileStorage.__file_path, mode="r", encoding="utf-8") as f:
                 FileStorage.__objects = json.load(f)
                 for key, value in FileStorage.__objects.items():
                     cls_name = value['__class__']
